e2c71d9183c7b0e445490dfa3a5f32271801f852
eriko <eriko@mppa.mp.br>
2018-11-28 12:47:30 -0300

    template literal e regexp

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -5,12 +5,12 @@ class DateConverter{
     }
 
     static paraTexto(data){
-            return data.getDate()
-            +'/'+ (data.getMonth()+1)
-            +'/'+ data.getFullYear();
+            return `${data.getDate()}/${(data.getMonth()+1)}/${data.getFullYear()}`;
     }
 
     static paraData(texto){
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+            throw new Error('Data deve estar no formato aaaa-mm-dd');
         return new Date(texto.split('-'))
     }
 }
