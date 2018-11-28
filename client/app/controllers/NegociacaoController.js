3aa38f1fda6f81b12d147a800ef8e8d15778914b
eriko <eriko@mppa.mp.br>
2018-11-28 11:20:23 -0300

    data

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,3 +1,34 @@
 class NegociacaoController{
+
+    constructor(){
+
+        let $ = document.querySelector.bind(document);
+        this._inputData =  $('#data');
+        this._inputQuantidade =  $('#quantidade');
+        this._inputValor =  $('#valor');
+        this._tbody = $('.table tbody');
+
+    }
+
+    adiciona (event){
+        event.preventDefault();
+        let data = new Date(this._inputData.value.split('-'));
+        console.log(data);
+        let negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        )
+            
+      //  console.log(negociacao);
+        this.limpaFormulario();
     
+    }
+
+    limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0;
+        this._inputData.focus();
+    }
 }
