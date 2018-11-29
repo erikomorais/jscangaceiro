e179c19d627d53b789c2219bf4d558ea43afb082
eriko <eriko@mppa.mp.br>
2018-11-29 10:44:57 -0300

    separação de responsabilidades

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -6,28 +6,29 @@ class NegociacaoController{
         this._inputData =  $('#data');
         this._inputQuantidade =  $('#quantidade');
         this._inputValor =  $('#valor');
-        this._tbody = $('.table tbody');
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update(this._negociacoes);
 
     }
 
     adiciona (event){
         event.preventDefault();
-        
-
-        let data = DateConverter.paraData(this._inputData.value);
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._negociacoes);
+        this._limpaFormulario();
        
-        let negociacao = new Negociacao(
-            data,
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
-        )
-
-        console.log(DateConverter.paraTexto(negociacao.data));
-        this.limpaFormulario();
     
     }
+    _criaNegociacao(){
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+    }
 
-    limpaFormulario(){
+    _limpaFormulario(){
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0;
